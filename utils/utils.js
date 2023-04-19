83dbaa95fb971b5d31d8807d83d1e5391173979e
Anastasia Kapustina <nikitinaan98@mail.ru>
2023-04-19 18:10:10 +0300

    fix: last err

--- utils/utils.js
+++ utils/utils.js
@@ -4,9 +4,11 @@ const DEFAULT_ERROR_CODE = 500;
 
 module.exports.checkError = (err, res) => {
   if (err.name === 'ValidationError') {
-    return res.status(VALIDATION_ERROR_CODE).send({ message: err.message });
+    return res.status(VALIDATION_ERROR_CODE).send({ message: 'Переданы некоректные данные' });
+  } if (err.name === 'NotFoundError') {
+    return res.status(NOTFOUND_ERROR_CODE).send({ message: err.message });
   } if (err.name === 'CastError') {
-    return res.status(NOTFOUND_ERROR_CODE).send({ message: 'Запрашиваемые данные не найдены' });
+    return res.status(VALIDATION_ERROR_CODE).send({ message: 'Запрашиваемые данные не найдены' });
   }
   return res.status(DEFAULT_ERROR_CODE).send({ message: 'На сервере произошла ошибка' });
 }
